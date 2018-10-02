e24b3b340f5528836dfe5a861e82975de4c5022d
Chien-Te Lee <Dexter.ChienTeLee@gmail.com>
2018-10-02 09:55:03 -0700

    replace vector pointer with shared pointer

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,5 +1,6 @@
 #include <AnalogSensor.hpp>
 #include <numeric>
+#include <memory>
 #include <vector>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
@@ -13,7 +14,8 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+    // change from vector pointer to shared pointer to automatically clear memory when finished
+    auto readings = std::make_shared<std::vector<int>> (mSamples, 10);
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
